0d47abfd8ec3df4811e8a7b9edd0902818228adb
Vishvesh Vijaywargiya <vishveshv@BGs-MacBook-Pro.Dlink>
2018-06-03 01:08:56 +0530

    syntax error fix

--- vars/python_compile.groovy
+++ vars/python_compile.groovy
@@ -1,4 +1,4 @@
 def call(String directory = '.')
 {
-	sh """find . -name *.py -print0|xargs -0 -L 1 python -m py_compile"""
+	sh """find ${directory} -name *.py -print0|xargs -0 -L 1 python -m py_compile"""
 }
