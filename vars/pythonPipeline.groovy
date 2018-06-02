24941b803c77199c738acb22e1ae96becf94d736
Vishvesh Vijaywargiya <vishveshv@BGs-MacBook-Pro.Dlink>
2018-06-03 01:20:27 +0530

    syntax error fix

---
+++ vars/pythonPipeline.groovy
@@ -0,0 +1,24 @@
+def call(String gitHubProject, String branch)
+{
+pipeline
+{
+  agent any
+  stages
+  {
+   stage('Clone')
+   {
+   	steps
+   	{
+   		checkout([$class: 'GitSCM', branches: [[name: '*/${branch}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'bg_local', url: "$gitHubProject"]]]) 
+   	}
+   }
+   stage('build')
+   {
+    steps
+     {
+       python_compile('python_proj_1')
+     }
+   }
+  }
+}
+}
